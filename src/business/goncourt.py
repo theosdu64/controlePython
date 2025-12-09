e182d9b40f855e41cda67481e122761d9c93fcf8
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2025-12-09 14:03:24 +0100

    ajout de la methode get_livre_by_id + print pour afficher lensemble de la donne dans le main comprenant ediiteur nom , auteur nom et prenom du livre

--- src/business/goncourt.py
+++ src/business/goncourt.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
-
+from typing import Optional, Any, Dict
 from src.daos.auteur_dao import AuteurDao
 from src.daos.jury_dao import JuryDao
+from src.daos.livre_dao import LivreDao
 from src.model.auteur import Auteur
 from src.model.jury import Jury
+from src.model.livre import Livre
 
 
 @dataclass
@@ -29,3 +31,14 @@ class Goncourt:
         """
         jury_dao : Jury = JuryDao()
         return jury_dao.read(id_jury)
+
+    @staticmethod
+    def get_livre_by_id(id_livre: int) -> Optional[Dict[str, Any]]:
+        """
+        Récupérer un livre a l'aide de son id
+
+        :param id_livre: int
+        :return: dict avec livre, auteur_nom, auteur_prenom, editeur_nom
+        """
+        livre_dao : Livre = LivreDao()
+        return livre_dao.read(id_livre)
--- src/main.py
+++ src/main.py
@@ -9,6 +9,16 @@ def main() -> None:
     print(Goncourt.get_auteur_by_id(11))
     print("-" * 60)
     print(Goncourt.get_jury_by_id(1))
+    print("-" * 60)
+    data = Goncourt.get_livre_by_id(1)
+    if data:
+        livre = data["livre"]
+        print(livre)
+        print(f"\nAuteur   : {data['auteur_prenom']} {data['auteur_nom']}")
+        print(f"Éditeur  : {data['editeur_nom']}")
+    else:
+        print("Livre non trouvé")
+
 
 if __name__ == "__main__":
     main()
