54a47b8e575a9e097139c7e65cf4a4f27a0abef8
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2025-12-09 10:18:52 +0100

    creation de la couche metier de lapplication goncourt  + creation static methode de recuperation de lauteur par son id

---
+++ src/business/goncourt.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+from src.daos.auteur_dao import AuteurDao
+from src.model.auteur import Auteur
+
+@dataclass
+class Goncourt:
+    """Couche métier de l'application de gestion du prix goncourt"""
+
+    @staticmethod
+    def get_auteur_by_id(id_auteur : int):
+        """Récupérer un auteur a l'aide de son id
+
+        :param : id_auteur : int
+        : return un Auteur(nom , prenom)
+        """
+
+        auteur_dao = AuteurDao
+        return auteur_dao.read(id_auteur)
--- src/daos/auteur_dao.py
+++ src/daos/auteur_dao.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe Auteur d'un livre
+"""
+
 import dataclasses
 from typing import Optional
 
