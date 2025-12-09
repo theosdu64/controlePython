b6b94fb0a34119492dd6d5f172ab102278863392
Theo SARHANE <SarhaneT@ad.fms-ea.com>
2025-12-09 11:50:36 +0100

    affichage de la methode get_jury_by_id

--- src/business/goncourt.py
+++ src/business/goncourt.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 
 from src.daos.auteur_dao import AuteurDao
+from src.daos.jury_dao import JuryDao
 from src.model.auteur import Auteur
+from src.model.jury import Jury
+
 
 @dataclass
 class Goncourt:
@@ -9,7 +12,6 @@ class Goncourt:
 
     @staticmethod
     def get_auteur_by_id(id_auteur : int) -> Auteur | None:
-        print(id_auteur)
         """Récupérer un auteur a l'aide de son id
 
         :param : id_auteur : int
@@ -17,3 +19,13 @@ class Goncourt:
         """
         auteur_dao : Auteur = AuteurDao()
         return auteur_dao.read(id_auteur)
+
+    @staticmethod
+    def get_jury_by_id(id_jury : int) -> Jury | None:
+        """Récupérer un jury a l'aide de son id
+
+        :param : id_jury : int
+        : return un Jury(nom , prenom, est_president ?)
+        """
+        jury_dao : Jury = JuryDao()
+        return jury_dao.read(id_jury)
--- src/main.py
+++ src/main.py
@@ -5,8 +5,10 @@ def main() -> None:
     --------------------------
     Bienvenue aux prix goncourt
     --------------------------""")
-
+    print("-" * 60)
     print(Goncourt.get_auteur_by_id(11))
+    print("-" * 60)
+    print(Goncourt.get_jury_by_id(1))
 
 if __name__ == "__main__":
     main()
